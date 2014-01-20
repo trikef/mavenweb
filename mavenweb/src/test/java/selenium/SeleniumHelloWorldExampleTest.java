4d72057ac1f72d807645b09e8abfbfaac102490e
kj <trikefile@yahoo.co.jp>
2014-01-20 19:42:16 +0900

    add selenium

---
+++ mavenweb/src/test/java/selenium/SeleniumHelloWorldExampleTest.java
@@ -0,0 +1,37 @@
+package selenium;
+
+import junit.framework.TestCase;
+
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.SeleniumException;
+
+public class SeleniumHelloWorldExampleTest extends TestCase {
+	private DefaultSelenium selenium;
+
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		selenium = createSeleniumClient("http://localhost:9090/");
+		selenium.start();
+	}
+
+	@Override
+	public void tearDown() throws Exception {
+		selenium.stop();
+		super.tearDown();
+	}
+
+	protected DefaultSelenium createSeleniumClient(String url) throws Exception {
+		return new DefaultSelenium("localhost", 4444, "*firefox", url);
+	}
+
+	public void testHelloWorld() throws Exception {
+		try {
+			selenium.open("http://localhost:9090/mavenweb/zero");
+			assertEquals("これがZero Configuration", selenium.getText("//h1"));
+		} catch (SeleniumException ex) {
+			fail(ex.getMessage());
+			throw ex;
+		}
+	}
+}
